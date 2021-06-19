7e1644e44f09c0c7e062db8325a98645363ad1cc
libinrct <q0411741@sina.com>
2021-06-19 21:19:07 +0800

    主场景资源替换  增加主角动画 增加音乐等

--- Homecoming/Assets/Scripts/Player.cs
+++ Homecoming/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //地图左侧墙壁 因觉得要用左侧进场 所以先不激活左侧墙壁
+    //等角色通过后再激活 防止角色走出地图
+    public GameObject leftWall;
     public Collider2D c2D;
     public Sprite sprite;
     public Animator animator;
@@ -13,26 +16,65 @@ public class Player : MonoBehaviour
     //是否在NPC附近，按下E键应不应该弹出对话框
     public bool talkable;
     public GameObject Talk;
+    //新游戏开始 播放开场动画 禁止玩家进行任何游戏内操作
+    public bool isStartGameAni = true;
+
+    AnimatorStateInfo ani;
     // Start is called before the first frame update
     void Start()
     {
         c2D = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+        //开始游戏 播放开场动画
+        PlayNewGame();
+        //Debug.Log(isStartGameAni);
+        leftWall.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (moveable)
+        if (isStartGameAni == false)
         {
-            float horizon = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * horizon * moveSpeed * Time.deltaTime);
+            if (moveable)
+            {
+                float horizon = Input.GetAxis("Horizontal");
+                transform.Translate(Vector3.right * horizon * moveSpeed * Time.deltaTime);
+               //Debug.Log(horizon);
+
+                if (horizon > 0.5f)
+                {
+                    animator.Play("playerRight");
+                    //Debug.Log("应该播放向右动画");
+                }
+                else if(horizon<-0.5f)
+                {
+                    animator.Play("playerLeft");
+                    //Debug.Log("应该播放向左动画");
+                }
+                else
+                {
+                    animator.Play("PlayerIdle");
+                    //Debug.Log("应该播放待机动画");
+
+                }
+            
+            }
+            if (Input.GetKeyDown(KeyCode.E) && talkable)
+            {
+                //TODO:清理对话框
+                BeginTalk();
+            }
         }
-        if(Input.GetKeyDown(KeyCode.E) && talkable)
+        //检测开成场动画是否播放完成
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime>=1.0f)
         {
-            //TODO:清理对话框
-            BeginTalk();
+            //开场动画播放完毕 可以控制角色
+            isStartGameAni = false;
+            //开场动画播放完毕 激活左侧墙壁
+            leftWall.SetActive(true);
         }
+        //Debug.Log(isStartGameAni + "   " + moveable + "   " + talkable);
     }
     public void BeginTalk()
     {
@@ -47,4 +89,11 @@ public class Player : MonoBehaviour
         moveable = true;
         Talk.SetActive(false);
     }
+    public void PlayNewGame()
+    {
+        animator.Play("playerAni");
+        //isStartGameAni = false;
+       
+    }
+
 }
